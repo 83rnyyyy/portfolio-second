a1d5a0ac9464b5c30f09ae3d096a057d3389ad78
Bernie <bernieliu2@gmail.com>
2025-08-29 06:31:26 -0400

    test

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -25,53 +25,7 @@ const About = () => {
           </div>
           <div className="absolute inset-x-0 pointer-evets-none -bottom-4 h-1/2 sm:h-1/3 bg-gradient-to-t from-indigo" />
         </div>
-        {/* Grid 2 */}
-        {/* <div className="grid-default-color grid-2">
-          <div
-            ref={grid2Container}
-            className="flex items-center justify-center w-full h-full"
-          >
-            <p className="flex items-end text-5xl text-gray-500">
-              Bernie Liu
-            </p>
-            <Card
-              style={{ rotate: "75deg", top: "30%", left: "20%" }}
-              text="DSA"
-              containerRef={grid2Container}
-            />
-            <Card
-              style={{ rotate: "-30deg", top: "60%", left: "45%" }}
-              text="Full Stack"
-              containerRef={grid2Container}
-            />
-            
-            <Card
-              style={{ rotate: "-45deg", top: "55%", left: "0%" }}
-              text="Python"
-              containerRef={grid2Container}
-            />
-            <Card
-              style={{ rotate: "20deg", top: "10%", left: "38%" }}
-              text="SRP"
-              containerRef={grid2Container}
-            />
-            <Card
-              style={{ rotate: "30deg", top: "70%", left: "70%" }}
-              image="assets/logos/cpp.svg"
-              containerRef={grid2Container}
-            />
-            <Card
-              style={{ rotate: "-45deg", top: "70%", left: "25%" }}
-              image="assets/logos/nextjs.svg"
-              containerRef={grid2Container}
-            />
-            <Card
-              style={{ rotate: "-45deg", top: "5%", left: "10%" }}
-              image="assets/logos/blazor-pink.png"
-              containerRef={grid2Container}
-            />
-          </div>
-        </div> */}
+        
         {/* Grid 3 */}
         <div className="grid-black-color grid-3">
           <div className="z-10 w-[50%]">
@@ -84,23 +38,76 @@ const About = () => {
             <Globe />
           </figure>
         </div>
-        {/* Grid 4 */}
+        
+        {/* Grid 4 - Email Button */}
         <div className="grid-special-color grid-2">
           <div className="flex flex-col items-center justify-center gap-4 size-full">
             <CopyEmailButton />
           </div>
         </div>
-        {/* Grid 5 */}
-        <div className="grid-default-color grid-4">
-          <div className="z-10 w-[50%]">
-            <p className="headText">Teck Stack</p>
-            <p className="subtext">
-              I specialize in a variety of languages, frameworks, and tools that
-              allow me to build robust and scalable applications
-            </p>
-          </div>
-          <div className="absolute inset-y-0 md:inset-y-9 w-full h-full start-[50%] md:scale-125">
-            <Frameworks />
+        
+        {/* Grid 5 - Tech Stack - Now spans full width */}
+        <div className="grid-default-color col-span-1 md:col-span-6">
+          <div className="flex flex-col md:flex-row h-full">
+            {/* Left side - Text content */}
+            <div className="z-10 w-full md:w-[40%] p-6 flex flex-col justify-center">
+              <p className="headText">Tech Stack</p>
+              <p className="subtext">
+                I specialize in a variety of languages, frameworks, and tools that
+                allow me to build robust and scalable applications
+              </p>
+              
+              {/* Tech categories list */}
+              <div className="mt-6 grid grid-cols-2 gap-4 text-sm">
+                <div>
+                  <h4 className="font-semibold text-white mb-2">Languages</h4>
+                  <ul className="text-gray-300 space-y-1">
+                    <li>JavaScript/TypeScript</li>
+                    <li>Python</li>
+                    <li>C++</li>
+                    <li>Java</li>
+                    <li>HTML/CSS</li>
+                  </ul>
+                </div>
+                <div>
+                  <h4 className="font-semibold text-white mb-2">Frontend</h4>
+                  <ul className="text-gray-300 space-y-1">
+                    <li>React</li>
+                    <li>Next.js</li>
+                    <li>Angular</li>
+                    <li>Bootstrap</li>
+                    <li>Tailwind CSS</li>
+                  </ul>
+                </div>
+                <div>
+                  <h4 className="font-semibold text-white mb-2">Backend</h4>
+                  <ul className="text-gray-300 space-y-1">
+                    <li>Node.js</li>
+                    <li>Flask</li>
+                    <li>FastAPI</li>
+                    <li>Spring</li>
+                    <li>PostgreSQL</li>
+                  </ul>
+                </div>
+                <div>
+                  <h4 className="font-semibold text-white mb-2">Tools</h4>
+                  <ul className="text-gray-300 space-y-1">
+                    <li>Git</li>
+                    <li>Vercel</li>
+                    <li>Jira</li>
+                    <li>Jenkins</li>
+                    <li>Three.js</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            
+            {/* Right side - Frameworks component */}
+            <div className="w-full md:w-[60%] h-full flex items-center justify-center">
+              <div className="w-full h-full scale-90 md:scale-100">
+                <Frameworks />
+              </div>
+            </div>
           </div>
         </div>
       </div>
